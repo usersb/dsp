fbca1e4f94ffcec19a7b38fb7fad89eea073d033
usersb <sonalbathe@gmail.com>
2017-03-30 11:37:02 -0700

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,21 @@
-PLACE YOUR CODE HERE
+import pandas as pd
+from collections import defaultdict
+
+faculty = pd.read_csv("faculty.csv", skipinitialspace=True)
+faculty['degree'] = faculty['degree'].str.replace('.','')
+
+#Q1. Find how many different degrees there are, and their frequencies: Ex: PhD, ScD, MD, MPH, BSEd, MS, JD, etc.
+def num_degrees(dataframe):
+    all_degrees = ' '.join(list(faculty['degree'])).split(' ')
+    #print(all_degrees)
+
+    count_dict = defaultdict(int)    
+    for degree in all_degrees:
+        count_dict[degree] += 1
+    no_degrees =  len(count_dict) -1  
+    print ("There are %d different degrees. ('0' is considered as not having a degree)" % no_degrees)
+    return count_dict
+
+degree_count_dict = num_degrees(faculty)
+print(degree_count_dict)
+
