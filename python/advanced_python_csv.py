1cc8194ff9bb4596b4b7b78749fcc608ce339961
usersb <sonalbathe@gmail.com>
2017-03-30 15:52:41 -0700

    Updated advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,10 @@
-PLACE YOUR CODE HERE
+def write_to_file(faculty):
+    email_list = print_emails(faculty)
+    with open('emails.csv', 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        
+        for email in email_list:
+            writer.writerow([email])
+        print("Done")
+        
+write_to_file(faculty)
