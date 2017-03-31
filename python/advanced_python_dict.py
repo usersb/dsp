c14e2ee28fa642687aa8098bd6c3fa9d50480b6d
usersb <sonalbathe@gmail.com>
2017-03-31 10:07:30 -0700

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -26,3 +26,38 @@ def create_dict(faculty):
 #print(print_n)
 print_n = list(mydict.items())
 print(print_n[:3])
+
+
+
+def create_tupkey_dict(faculty):
+    first_last_name = []
+    for name1 in faculty['name']:
+        splitname = name1.split()
+        if len(splitname) == 3:
+            fname = name1.split()[-3]
+            lname = name1.split()[-1]
+        elif len(splitname) == 2:
+            fname = name1.split()[-2]
+            lname = name1.split()[-1]
+        fl_name = [fname, lname]
+        t = tuple(fl_name)
+        first_last_name.append(t)
+
+
+    faculty_tup = faculty
+    faculty_tup_df = pd.DataFrame(faculty_tup)
+    faculty_tup_df['name'] =  first_last_name   
+
+    tup_dict = {}
+    for x in range(len(faculty_tup_df)):
+        currentid = faculty_tup_df.iloc[x,0]
+        degree = faculty_tup_df.iloc[x,1]
+        title = faculty_tup_df.iloc[x,2]
+        email = faculty_tup_df.iloc[x,3]
+        tup_dict.setdefault(currentid, [])
+        tup_dict[currentid].append(degree)
+        tup_dict[currentid].append(title)
+        tup_dict[currentid].append(email)
+    
+    print(tup_dict)
+    
