c4b70bbcf36f026819cdf5b51cd3703aad8bfaa0
usersb <sonalbathe@gmail.com>
2017-04-01 07:41:48 -0700

    Updated matrix_algebra.py

--- math/matrix_algebra.py
+++ math/matrix_algebra.py
@@ -6,7 +6,15 @@ a = np.matrix( ((1,2,3), (2,7,4)) )
 b = np.matrix( ((1,-1), (0, 1)) )
 c = np.matrix(((5,-1), (9,1), (6,0)))
 d = np.matrix(((3,-2,-1), (1,2,3)))
+u = np.array([6,2,-3,5], ndmin = 2)
+v = np.array([3,5,-1,4], ndmin = 2)
+w = np.array([[1],
+             [8],
+             [0],
+             [5]], ndmin = 2)
+alpha = 6
 
+#Matrix Dimensions
 
 #1.1)
 print(a.shape)
@@ -24,7 +32,37 @@ print(c.shape)
 print(d.shape)
 #(2, 3)
 
+#1.5)
+print(u.shape)
+#(1, 4)
 
+#1.6)
+print(w.shape)
+#(4, 1)
+
+# Vector Operations
+
+#2.1)
+print(u + v)
+#[[ 9  7 -4  9]]
+
+#2.2)
+print(u - v)
+#[[ 3 -3 -2  1]]
+
+#2.3)
+print(alpha * u)
+#[[ 36  12 -18  30]]
+
+#2.4)
+#print(np.dot(u, v))  (not defined)
+
+#2.5)
+print(np.linalg.norm(u))
+#8.60232526704
+
+
+# Matrix Operations
 
 #3.1)
 #a + c (not defined)
